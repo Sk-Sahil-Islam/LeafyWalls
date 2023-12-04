da2ce36ae6c656514c89d31bb5df8a66c060779e
Sk-Sahil-Islam <sksahilislam0123@gmail.com>
2023-12-04 21:18:30 +0530

    m3 version change and tabrow in home screen

--- app/src/main/java/com/example/leafywalls/ui/theme/Color.kt
+++ app/src/main/java/com/example/leafywalls/ui/theme/Color.kt
@@ -10,7 +10,7 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val BackgroundDark = Color(0xFF151B0F)
-val BackgroundLight = Color(0xFFFAFFEF)
+val BackgroundLight = Color(0xFFFCFFF5)
 
 val PrimaryDark = Color(0xFFAEF359)
-val PrimaryLight = Color(0xFF85C536)
+val PrimaryLight = Color(0xFF5F8A2B)
